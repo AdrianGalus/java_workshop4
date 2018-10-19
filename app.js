11c7316a38fb067ca24c7cc67b70bfdd48dc8c04
datny <robert.przydatek.cl@gmail.com>
2018-10-19 16:23:53 +0200

    added id

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 $(function() {
 
     var titles = $("#titles");
+    titles.on("click","li",function(){
+       $(this).find("div").slideToggle();
+       var bookid = this.dataset.id;
+        console.log(bookid);
+        $.ajax({url:"http://localhost:8282/books/"})
+
+
+
+    });
     $.ajax({
         url: "http://localhost:8282/books/"})
         .done(function(result) {console.log(result);
         $(result).each(function(index, element) {
-            titles.append($(`<li>${$(this).title}<div></div></li>`))
+
+            titles.append($(`<li data-id="${this.id}">${this.title}<div style="display:none"></div></li>`))
+
         })
+
         })
 });
