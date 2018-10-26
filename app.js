4c46aacb6ab143953e86a87ee38f46e21153ccbc
Piotr <piotrgoralewicz25@gmail.com>
2018-10-26 16:04:26 +0200

    dodawanie nowej kśiążki

--- app.js
+++ app.js
@@ -1,28 +1,55 @@
 $(function() {
 
+    var form = $("#addBook");
+
+
+
+
+    form.on("submit", function(event) {
+
+        var title = $("#title").val();
+        var author = $("#author").val();
+        var publisher = $("#publisher").val();
+        var type = $("#type").val();
+        var isbn = $("#isbn").val();
+
+        var dataToSend = {
+            title: title,
+            author: author,
+            publisher: publisher,
+            type: type,
+            isbn: isbn
+        };
+
+        console.log(title);
+        $.ajax({
+            url: "http://localhost:8282/books/",
+            data: JSON.stringify(dataToSend),
+            type: "POST",
+            contentType: "application/json"
+        }).done(function(result) {
+        })
+        event.preventDefault();
+        console.log(JSON.stringify(dataToSend));
+    });
     var titles = $("#titles");
     titles.on("click","li",function(){
-      var div = $(this).find("div");
-      div.slideToggle();
-       var bookid = this.dataset.id;
+        var div = $(this).find("div");
+        div.slideToggle();
+        var bookid = this.dataset.id;
         console.log(bookid);
         $.ajax({url:"http://localhost:8282/books/"+ bookid})
             .done(function (data) {
                 console.log(data);
                 div.text("Autor: " + data.author + ", Wydawnictwo: " +  data.publisher + ", Rodzaj: " + data.type);
             })
-
-
-
     });
     $.ajax({
         url: "http://localhost:8282/books/"})
         .done(function(result) {console.log(result);
         $(result).each(function(index, element) {
 
-            titles.append($(`<li data-id="${this.id}">${this.title}<div style="display:none"></div></li>`))
-
+            titles.append($(`<li data-id="${this.id}">${this.title}<div style="display:none"></div></li>`));
         })
-
         })
 });
