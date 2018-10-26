ee5e9afd964175fd28c412aca37b2257c6228999
AdrianGalus <adriangalus8@gmail.com>
2018-10-26 15:12:03 +0200

    last changes

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 $(function() {
 
+    var form = $("#addBook");
+    form.on("submit", function(event) {
+        event.preventDefault();
+    });
     var titles = $("#titles");
     titles.on("click","li",function(){
-      var div = $(this).find("div");
-      div.slideToggle();
-       var bookid = this.dataset.id;
+        var div = $(this).find("div");
+        div.slideToggle();
+        var bookid = this.dataset.id;
         console.log(bookid);
         $.ajax({url:"http://localhost:8282/books/"+ bookid})
             .done(function (data) {
                 console.log(data);
                 div.text("Autor: " + data.author + ", Wydawnictwo: " +  data.publisher + ", Rodzaj: " + data.type);
             })
-
-
-
     });
     $.ajax({
         url: "http://localhost:8282/books/"})
         .done(function(result) {console.log(result);
         $(result).each(function(index, element) {
 
-            titles.append($(`<li data-id="${this.id}">${this.title}<div style="display:none"></div></li>`))
-
+            titles.append($(`<li data-id="${this.id}">${this.title}<div style="display:none"></div></li>`));
         })
-
         })
 });
